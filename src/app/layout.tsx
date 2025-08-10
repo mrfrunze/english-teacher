5eb50e210f8d31e7e6d80eeb24b17ae69d6d2b32
Ilya <stoynov.oleg@gmail.com>
2025-08-10 17:14:32 +0200

    commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,61 @@
+import type { Metadata } from "next";
+import {League_Spartan, Poppins } from "next/font/google";
+import "./globals.css";
+import JsonLd from "@/components/JsonLd";
+
+const leagueSpartan = League_Spartan({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--ff-league_spartan",
+  display: "swap",
+})
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--ff-poppins",
+  display: "swap",
+})
+
+export const metadata: Metadata = {
+  title: "English Lessons with a Teacher — Individual and Friendly",
+  description: "Online English lessons for Beginner, Pre-Intermediate, and Intermediate levels. Individual approach, exam preparation, flexible schedule.",
+
+  openGraph: {
+    title: "English Lessons with a Teacher",
+    description:
+      "Online English lessons for beginners and exam preparation.",
+    url: "",
+    siteName: "English Teacher",
+    images: [
+      {
+        url: "", // добавим в public позже
+        width: 1200,
+        height: 630,
+        alt: "English lessons online",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${leagueSpartan.variable} ${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+      <main>
+          <h1 className="font-poppins uppercase">poppins</h1>
+          <h2 className="font-sparta text-2xl">sparta</h2>
+      </main>
+      <footer className="">
+        
+      </footer>
+    </div>
+  );
+}
